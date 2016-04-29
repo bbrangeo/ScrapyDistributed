50f1723cdc9199cae39a0a7f9e393b2dbd7204aa
cqc <1016903103@qq.com>
2016-04-29 23:36:50 +0800

    remove extra import

--- scrapy_distributed/webui/core/views.py
+++ scrapy_distributed/webui/core/views.py
@@ -1,16 +1,13 @@
 from datetime import datetime
 import json
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
-from django.template import Context
 from .models import Spider, Rule
 from libs.create_model import create_spider as cs, create_rule as cr
 from libs.update_model import update_spider as us, update_rule as ur
 from libs.delete_model import delete_spider as ds, delete_rule as dr
 from scrapy_distributed.webui.core.libs.common import get_ip, get_hash
 from scrapy_distributed.webui.lib.settings import REDIS as redis
-from django.utils.safestring import SafeString
 
 def index(request):
     spiders = Spider.objects.order_by('updated_at')
