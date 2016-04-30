17695bc35903e3e573bd7f34a11eaa65c8697862
cqc <1016903103@qq.com>
2016-04-30 23:36:43 +0800

    update mongodb

--- scrapy_distributed/mongodb/connection.py
+++ scrapy_distributed/mongodb/connection.py
@@ -1,20 +1,21 @@
-import redis
+from pymongo import MongoClient
+
 
 
 # Default values.
-REDIS_URL = None
-REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
-REDIS_PASSWORD = ''
+MONGODB_URL = None
+MONGODB_HOST = 'localhost'
+MONGODB_USER = 'root'
+MONGODB_PASSWORD = ''
 
 def from_settings(settings):
-    url = settings.get('REDIS_URL',  REDIS_URL)
-    host = settings.get('REDIS_HOST', REDIS_HOST)
-    port = settings.get('REDIS_PORT', REDIS_PORT)
-    password = settings.get('REDIS_PASSWORD', REDIS_PASSWORD)
+    url = settings.get('MONGODB_URL',  MONGODB_URL)
+    host = settings.get('MONGODB_HOST', MONGODB_HOST)
+    user = settings.get('MONGODB_USER', MONGODB_USER)
+    password = settings.get('MONGODB_PASSWORD', MONGODB_PASSWORD)
 
-    # REDIS_URL takes precedence over host/port specification.
     if url:
-        return redis.from_url(url)
+        return MongoClient(url)
     else:
-        return redis.Redis(host=host, port=port, password=password)
+        url = 'mongodb://{0}:{1}@{2}'.format(user, password, host)
+        return MongoClient(url)
